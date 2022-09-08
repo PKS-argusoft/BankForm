afa4cea450a0bd97cf85e869bb0a0e9c5e0d0cca
Paresh Sethy <psethy@argusoft.com>
2022-09-09 02:22:24 +0530

    last commit of day

--- BankFormWeb/Controllers/TemplateController.cs
+++ BankFormWeb/Controllers/TemplateController.cs
@@ -42,5 +42,82 @@ public class TemplateController : Controller
         return View(obj);
     }
 
+    //get
+    public IActionResult Edit(int? id )
+    {
+        if(id == null || id==0)
+        {
+            return NotFound();
+        }
+        var templateFromDb = _db.Templates.Find(id);
+
+        if(templateFromDb == null)
+        {
+            return NotFound();
+        }
+
+        TempData["old_name_store"] = templateFromDb.TemplateName;
+
+        return View(templateFromDb);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Template obj)
+    {
+        if (ModelState.IsValid)
+        {
+            obj.CreatedAt = obj.CreatedAt;
+            obj.UpdatedAt = System.DateTime.Now;
+
+            _db.Templates.Update(obj);
+            _db.SaveChanges();
+            TempData["Success"] = TempData["old_name_store"] + " has been edited to " + obj.TemplateName + " successfully . ";
+            return RedirectToAction("Index");
+
+        }
+        return View(obj);
+    }
+
+
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var TemplateFromDb = _db.Templates.Find(id);
+        //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+        //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+        if (TemplateFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(TemplateFromDb);
+    }
+
+    //POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var obj = _db.Templates.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+
+        _db.Templates.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Template deleted successfully";
+        return RedirectToAction("Index");
+
+    }
+
+
 
 }
