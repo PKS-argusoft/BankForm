3ba934d6a7e9194ecd2de5be69baff26cd45f797
Paresh Sethy <psethy@argusoft.com>
2022-09-08 23:24:51 +0530

    first commit

---
+++ BankFormWeb/Controllers/TemplateController.cs
@@ -0,0 +1,46 @@
+﻿using BankFormWeb.Data;
+using BankFormWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankFormWeb.Controllers;
+
+public class TemplateController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public TemplateController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var objTemplateList = _db.Templates.ToList();
+
+        return View(objTemplateList);
+    }
+
+    //GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //post
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Template obj)
+    {
+        if(ModelState.IsValid)
+        {
+            obj.CreatedAt = System.DateTime.Now;
+            _db.Templates.Add(obj);
+            _db.SaveChanges();
+            TempData["Success"] = obj.TemplateName + "is added successfully .";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+
+}
---
+++ BankFormWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BankFormWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankFormWeb.Data;
+
+public class ApplicationDbContext:DbContext 
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+    {
+
+    }
+
+    public DbSet<Template>  Templates { get; set; }
+
+}
