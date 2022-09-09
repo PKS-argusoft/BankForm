ead0600997d9c817d0a69c18c2a3a780f0ad8717
Paresh Sethy <psethy@argusoft.com>
2022-09-10 02:22:57 +0530

    repository implementation started

---
+++ BankForm.DataAccess/Repository/IRepository/ITemplateRepository.cs
@@ -0,0 +1,14 @@
+﻿using BankForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankForm.DataAccess.Repository.IRepository
+{
+    internal interface ITemplateRepository : IRepository<Template>
+    {
+        void Update(Template obj);
+    }
+}
